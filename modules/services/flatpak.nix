faba70b350bc4a423258eeebabd003701058e47c
István Urbán <51445152+istvnurbn@users.noreply.github.com>
2026-08-17 22:31:38 +0200

    fix: flatpak-repo.service properly reports "done" state

--- modules/services/flatpak.nix
+++ modules/services/flatpak.nix
@@ -16,6 +16,10 @@
       systemd.services.flatpak-repo = {
         wantedBy = ["multi-user.target"];
         path = [pkgs.flatpak];
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
         script = ''
           flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
         '';
