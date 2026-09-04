679f3a18cfbe12c9ca4884134857739e51a02775
István Urbán <51445152+istvnurbn@users.noreply.github.com>
2026-09-04 16:38:36 +0200

    feat: clean up tmp on boot

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,22 +1,26 @@
-{
+{lib, ...}: {
   den.aspects.boot = {
-    nixos.boot.loader = {
-      # Use the systemd-boot EFI boot loader.
-      systemd-boot = {
-        enable = true;
-        configurationLimit = 3;
-        # Make Memtest86+ available from the systemd-boot menu
-        memtest86 = {
+    nixos.boot = {
+      loader = {
+        # Use the systemd-boot EFI boot loader.
+        systemd-boot = {
           enable = true;
-          sortKey = "o_memtest86";
+          configurationLimit = 3;
+          # Make Memtest86+ available from the systemd-boot menu
+          memtest86 = {
+            enable = true;
+            sortKey = "o_memtest86";
+          };
+          # netboot.xyz allows you to boot OS installers and utilities over the network
+          netbootxyz = {
+            enable = true;
+            sortKey = "o_netbootxyz";
+          };
         };
-        # netboot.xyz allows you to boot OS installers and utilities over the network
-        netbootxyz = {
-          enable = true;
-          sortKey = "o_netbootxyz";
-        };
+        efi.canTouchEfiVariables = true;
       };
-      efi.canTouchEfiVariables = true;
+
+      tmp.cleanOnBoot = lib.mkDefault true;
     };
   };
 }
